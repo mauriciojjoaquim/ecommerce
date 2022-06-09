4e79208d7741d74cd6c693e0cb4e698ad8733a87
mauriciojjoaquim <mauriciojjoaquim@msn.com>
2022-06-09 20:39:41 -0300

    inicio do projeto loja virtual trste sql

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit2fbba6b1cabf7184e606a8eda539ba10
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInit2fbba6b1cabf7184e606a8eda539ba10
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit2fbba6b1cabf7184e606a8eda539ba10::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit2fbba6b1cabf7184e606a8eda539ba10::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit2fbba6b1cabf7184e606a8eda539ba10::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit2fbba6b1cabf7184e606a8eda539ba10::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit2fbba6b1cabf7184e606a8eda539ba10::$classMap;
 
