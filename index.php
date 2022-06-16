26baa4cd346919ddf7785b16685be5cfc85cdcb1
mauriciojjoaquim <mauriciojjoaquim@msn.com>
2022-06-16 11:58:08 -0300

    teplete login

--- index.php
+++ index.php
@@ -1,29 +1,98 @@
 <?php 
-
+use Hcode\PageAdmin;
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
+use Hcode\Model\User;
 
-$app = new Slim();
+$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function(){
-
-	$page = new Page();
+$app->get('/', function() {
+    
+	$page = new Hcode\Page();
 
 	$page->setTpl("index");
 
 });
-$app->get('/admin', function(){
 
+$app->get('/admin', function() {
+    
+	User::verifyLogin();
+
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
+});
+$app->get("/admin/users", function(){
+
+		user::verifyLogin();
+		$page = new PageAdmin();
+		$page->setTpl("users");
+
+});
+$app->get("/admin/users/create", function(){
+
+	user::verifyLogin();
 	$page = new PageAdmin();
-
-	$page->setTpl("index");
+	$page->setTpl("users-create");
 
 });
+$app->get("/admin/users/:iduser", function($iduser){
+
+	user::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl("users-update");
+
+});
+$app->post("/admin/users/create", function(){
+
+	user::verifyLogin();
+
+});
+$app->post("/admin/users/:iduser", function($isuser){
+
+	user::verifyLogin();
+
+});
+$app->delete("/admin/users/:iduser", function($isuser){
+
+	user::verifyLogin();
+
+});
+ 
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -6,50 +6,66 @@ use Rain\Tpl;
 
 class Page {
 
-    private $tpl;
-    private $options = [];
-    private $default = [
-        "data"=>[]
-    ];
+	private $tpl;
+	private $options = [];
+	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
+		"data"=>[]
+	];
 
-    public function __construct($opts = array(), $tpl_dir = "/views/"){
+	public function __construct($opts = array(), $tpl_dir = "/views/")
+	{
 
-        $this->options = array_merge($this->default, $opts);
+		$this->defaults["data"]["session"] = $_SESSION;
 
-       
-    $config = array(
-                    "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-                    "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-                    "debug"         => false 
-                   );
+		$this->options = array_merge($this->defaults, $opts);
 
-    Tpl::configure( $config );
+		$config = array(
+		    "base_url"      => null,
+		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
+		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
+		    "debug"         => false
+		);
 
-    $this->tpl = new Tpl;
+		Tpl::configure( $config );
 
-    $this->setData($this->options["data"]);
+		$this->tpl = new Tpl();
 
-    $this->tpl->draw("header");
+		if ($this->options['data']) $this->setData($this->options['data']);
 
-    }
-    private function setData($data = array()){
+		if ($this->options['header'] === true) $this->tpl->draw("header", false);
 
-        foreach ($data as $key => $value) {
-        
-        $this->tpl->assign($key, $value);
-         }
-    }
-    public function setTpl($name, $data = array(), $returnHTML = false) {
+	}
 
-            $this->setData($data);
-            return $this->tpl->draw($name, $returnHTML);
+	public function __destruct()
+	{
 
-    }
-    public function __destruct(){
+		if ($this->options['footer'] === true) $this->tpl->draw("footer", false);
 
-        $this->tpl->draw("footer");
+	}
 
-    }
+	private function setData($data = array())
+	{
+
+		foreach($data as $key => $val)
+		{
+
+			$this->tpl->assign($key, $val);
+
+		}
+
+	}
+
+	public function setTpl($tplname, $data = array(), $returnHTML = false)
+	{
+
+		$this->setData($data);
+
+		return $this->tpl->draw($tplname, $returnHTML);
+
+	}
 
 }
+
  ?>
